6156e2cb3ce8639310cdeca55854bc46e2292af6
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2024-06-26 11:13:12 +0200

    chore: add DrupalAttribute toString test
    Add jest to test DrupalAttribute's `toString` method. For this, `DrupalAttribute` was moved into its own file so it can be imported in the test without adding it to the export of index.js.

---
+++ drupal-attribute.test.js
@@ -0,0 +1,24 @@
+const { describe, expect, test } = require("@jest/globals");
+
+const { DrupalAttribute } = require("./drupal-attribute.js");
+
+describe("DrupalAttribute", () => {
+  test.each([
+    [
+      {
+        // The special `$drupal` attribute should be ignored.
+        $drupal: true,
+        "data-selector": "",
+        "aria-controls": "test",
+        id: "test",
+        "single-space": " ",
+        "boolean-true": true,
+        "boolean-false": false,
+      },
+      ' data-selector="" aria-controls="test" id="test" single-space=" " boolean-true="true" boolean-false="false"'
+    ],
+  ])("toString", (attributes, htmlAttributeString) => {
+    const drupalAttribute = new DrupalAttribute(Object.entries(attributes))
+    expect(drupalAttribute.toString()).toEqual(htmlAttributeString);
+  });
+});
